0ac33a9478383b985a3608dfcb6e0c9e69f4c6c1
Carolyn Huynh <ckhuynh20@gmail.com>
2014-08-15 14:49:48 -0700

    create method to update customer name in database

--- lib/customers.rb
+++ lib/customers.rb
@@ -1,5 +1,4 @@
 class Customer
-
 	attr_reader :id, :name
 
 	def initialize attributes
@@ -7,4 +6,27 @@ class Customer
 		@name = attributes['name']
 	end
 
+	def == another_customer
+		self.name == another_customer.name &&
+		self.id == another_customer.id
+	end
+
+	def self.all
+		results = DB.exec("SELECT * FROM customers;")
+		customers = []
+		results.each do |result|
+		 	customers << Customer.new(result)
+		 end
+		customers
+	end
+
+	def save
+		results = DB.exec("INSERT INTO customers (name) VALUES ('#{@name}') RETURNING id;")
+		@id = results.first['id'].to_i
+	end
+
+	def update_name(new_name)
+		@name = new_name
+		DB.exec("UPDATE customers SET name = '#{new_name}' WHERE id = #{@id};")
+	end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -6,4 +6,26 @@ describe Customer do
 		expect(test_customer).to be_an_instance_of Customer
 		expect(test_customer.name).to eq 'Boo Radley'
 	end
+
+	it 'should save the name to the database' do
+		test_customer = Customer.new({'name' => 'Boo Radley'})
+		test_customer.save
+		expect(Customer.all).to eq [test_customer]
+	end
+
+	it 'should update the customer name' do
+		test_customer = Customer.new({'name' => 'Boo Radley'})
+		test_customer.save
+		new_name = "Atticus Finch"
+		test_customer.update_name(new_name)
+		expect(test_customer.name).to eq "Atticus Finch"
+		expect(Customer.all).to eq [test_customer]
+	end
+
+	it 'should delete the customer from the database' do
+		test_customer = Customer.new({'name' => 'Boo Radley'})
+		test_customer.save
+		test_customer.delete
+		expect(Customer.all).to eq []
+	end
 end
