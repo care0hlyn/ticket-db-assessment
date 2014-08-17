dea13ac4a77c82a28aced004df388fc4ea6aac38
Carolyn Huynh <ckhuynh20@gmail.com>
2014-08-16 20:51:00 -0700

    create UI page to take customer name and order

--- ui.rb
+++ ui.rb
@@ -1,19 +1,23 @@
 require './lib/customers'
 require './lib/menu'
+require 'pg'
+
+DB = PG.connect(:dbname => 'ticket')
+
+@customer
 
 def main_menu
-
 	puts " \n\n--- KITCHEN TICKET APP---\n\n"
 	puts " '1' for customer menu."
 	puts " '2' for food menu."
 	puts " 'x' to exit program."
 
 	case user_decision = gets.chomp
-	when user_decision == '1'
+	when '1'
 		customer_menu
-	when user_decision == '2'
+	when '2'
 		food_menu
-	when user_decision == 'x'
+	when 'x'
 		puts "Goodbye, Alfred."
 		exit
 	else 
@@ -24,31 +28,56 @@ end
 
 
 def customer_menu
-
 	puts " \n\n--- CUSTOMER MENU ---\n\n"
 	puts " '1' to create new customer."
-	puts " '2' for order food."
+	puts " '2' to order food."
 	puts " 'x' to return back to main menu."
 
 	case user_decision = gets.chomp
-	when user_decision == '1'
+	when '1'
 		create_customer
-	when user_decision == '2'
+	when '2'
 		order_food
-	when user_decision == 'x'
-		puts "Goodbye Alfred."
+	when 'x'
+		puts "Goodbye, Alfred."
 		main_menu
 	else
 		puts "Invalid."
 		main_menu
 	end
-
 end
 
 def create_customer
 	puts "Enter new name:"
 	user_decision = gets.chomp
-	@customer = Customer.new({:name => user_decision})
+	@customer = Customer.new({'name' => user_decision})
+	@customer.save
+
+	puts "#{@customer.name} has been saved."
+
+	puts "\n\n Would you like to add an order, #{@customer.name} Y/N?\n\n"
+
+	case user_decision = gets.chomp.downcase
+	when 'y'
+		order_food
+	when 'n'
+		puts "Goodbye, Alfred."
+	else 
+		puts "Invalid."
+		main_menu
+	end
+end
+
+def order_food
+	puts "Enter your dinner:"
+	user_decision = gets.chomp
+	order = Order.new({'food_item' => user_decision})
+	order.save
+	Order.ticket(@customer.id, order.id)
+	puts "#{@customer.name}, your order of #{order.food_item} has been placed."
+
+
+
 
 end
 
